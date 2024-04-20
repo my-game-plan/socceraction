8206ee0e715a720bfeb9090af91ac89ecf7f5ef7
Lode Van Tente <lode.van.tente@gmail.com>
2024-04-20 11:06:50 +0200

    Add tests for Hybrid-VAEP

---
+++ tests/hybrid_vaep/test_hybrid_vaep.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import pytest
+
+from socceraction.hybrid_vaep import HybridVAEP
+from socceraction.vaep import features as fs
+
+
+@pytest.fixture(scope='session')
+def vaep_model(sb_worldcup_data: pd.HDFStore) -> HybridVAEP:
+    # Test the vAEP framework on the StatsBomb World Cup data
+    model = HybridVAEP(nb_prev_actions=1)
+    # comppute features and labels
+    games = sb_worldcup_data['games']
+    features = pd.concat(
+        [
+            model.compute_features(game, sb_worldcup_data[f'actions/game_{game.game_id}'])
+            for game in games.iloc[:-1].itertuples()
+        ]
+    )
+    expected_features = set(fs.feature_column_names(model.xfns_standard, model.nb_prev_actions))
+    assert set(features.columns) == expected_features
+
+    labels = pd.concat(
+        [
+            model.compute_labels(game, sb_worldcup_data[f'actions/game_{game.game_id}'])
+            for game in games.iloc[:-1].itertuples()
+        ]
+    )
+    expected_labels = {'scores', 'concedes'}
+    assert set(labels.columns) == expected_labels
+    assert len(features) == len(labels)
+    # fit the model
+    model.fit(features, labels)
+    return model
+
+
+@pytest.mark.e2e
+def test_predict(sb_worldcup_data: pd.HDFStore, vaep_model: HybridVAEP) -> None:
+    games = sb_worldcup_data['games']
+    game = games.iloc[-1]
+    actions = sb_worldcup_data[f'actions/game_{game.game_id}']
+    ratings = vaep_model.rate(game, actions)
+    expected_rating_columns = {'offensive_value', 'defensive_value', 'vaep_value'}
+    assert set(ratings.columns) == expected_rating_columns
+
+
+@pytest.mark.e2e
+def test_predict_with_missing_features(sb_worldcup_data: pd.HDFStore, vaep_model: HybridVAEP) -> None:
+    games = sb_worldcup_data['games']
+    game = games.iloc[-1]
+    actions = sb_worldcup_data[f'actions/game_{game.game_id}']
+    X = vaep_model.compute_features(game, actions)
+    del X['period_id_a0']
+    with pytest.raises(ValueError):
+        vaep_model.rate(game, actions, X)
